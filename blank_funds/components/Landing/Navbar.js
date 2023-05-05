b74dbd212c362fc1c5c827f8672aced4e036765b
Anshuman Swain <anshuman8swain@gmail.com>
2023-05-05 15:48:26 +0530

    navbar items updated

--- blank_funds/components/Landing/Navbar.js
+++ blank_funds/components/Landing/Navbar.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import {BsFillGridFill} from 'react-icons/bs';
+import { useRouter } from 'next/router';
 
 export default function Navbar() {
+    const router = useRouter();
+
   return (
     <div className='flex py-10'>
         <div className='flex-none'>
             <BsFillGridFill size={22}/>
         </div>
-        <div className='grow flex flex-row justify-center space-x-20'>
-            <div>Home</div>
-            <div>About</div>
-            <div>Products</div>
-            <div>Home</div>
-            <div>About</div>
-            <div>Products</div>
+        <div className='grow flex flex-row justify-center space-x-20 font-medium'>
+            <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3'>Home</div>
+            <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3' onClick={()=>router.push("/listings")}>Organisations</div>
+            <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3'>Stories</div>
+            <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3'>About</div>
+            <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3'>Profile</div>
         </div>
     </div>
   )
---
+++ blank_funds/pages/listings.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function listings() {
+  return (
+    <div className="container mx-auto px-8 md:px-16">
+      
+  </div>
+  )
+}
