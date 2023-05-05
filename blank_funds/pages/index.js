cf6f9efd826d9dedda89561ada8101bdbceb4914
Sahithya <aythihas2003@gmail.com>
2023-05-05 12:42:40 +0530

    add custom colours

--- blank_funds/pages/index.js
+++ blank_funds/pages/index.js
@@ -1,18 +1,7 @@
-import { motion } from "framer-motion";
-
 export default function Home() {
   return (
-    <div className="container mx-auto px-8 md:px-16">
-      <motion.div
-        whileHover={{ scale: 1.2, rotate: 90 }}
-        whileTap={{
-          scale: 0.8,
-          rotate: -90,
-          borderRadius: "100%",
-        }}
-      >
-      <div className="bg-red">hellow</div>
-      </motion.div>
+    <div className="container mx-auto px-8 md:px-16 bg-primary">
+      hello
     </div>
   );
 }
