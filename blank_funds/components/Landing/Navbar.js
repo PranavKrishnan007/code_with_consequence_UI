50ea0147c7ed3ddc16bff198950fc1a4b5d4ee06
Pranavkrishnan007 <pranavk0217@gmail.com>
2023-05-07 05:15:43 +0530

    fix navbar

--- blank_funds/components/Landing/Navbar.js
+++ blank_funds/components/Landing/Navbar.js
@@ -5,18 +5,18 @@ import { useRouter } from 'next/router';
 export default function Navbar() {
     const router = useRouter();
 
-  return (
-    <div className='flex py-10 fixed z-50 bg-background w-screen drop-shadow-3xl px-28 '>
-        <div className='flex-none'>
-            <BsFillGridFill size={22}/>
+    return (
+        <div className='flex py-10 sticky top-0 z-50 bg-background w-screen drop-shadow-3xl px-28 '>
+            <div className='flex-none'>
+                <BsFillGridFill size={22}/>
+            </div>
+            <div className='grow flex flex-row justify-center space-x-20 font-medium'>
+                <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3'>Home</div>
+                <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3' onClick={()=>router.push("/listings")}>Organisations</div>
+                <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3'>Stories</div>
+                <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3'>About</div>
+                <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3'>Profile</div>
+            </div>
         </div>
-        <div className='grow flex flex-row justify-center space-x-20 font-medium'>
-            <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3'>Home</div>
-            <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3' onClick={()=>router.push("/listings")}>Organisations</div>
-            <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3'>Stories</div>
-            <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3'>About</div>
-            <div className='border-b-2 border-transparent hover:border-primary cursor-pointer px-3'>Profile</div>
-        </div>
-    </div>
-  )
+    )
 }
