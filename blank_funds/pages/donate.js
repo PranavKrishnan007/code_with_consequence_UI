c0477818fccc968895c3c1779fb14625981fb1a0
Pranavkrishnan007 <pranavk0217@gmail.com>
2023-05-07 08:30:55 +0530

    transaction page

--- blank_funds/pages/donate.js
+++ blank_funds/pages/donate.js
@@ -1,5 +1,6 @@
 import Navbar from '@/components/Landing/Navbar';
 import bg from '/public/assets/heart_donation.png';
+import styles from '../styles/Donation.module.css'
 
 export default function Donate () {
   return (
@@ -8,8 +9,14 @@ export default function Donate () {
       <div className="containerCSS flexySide paddingDonate heightDonate" style={{width: "100%"}}>
         {/*still need to fix the import of the image as the background*/}
         <div style={{width: "65%", height: "100%", backgroundSize: 'cover', backgroundImage: `url(${bg})`, }} />
-        <div style={{width: "35%", background: "#DBA39A"}}>
-          dsgsdgasdfgaf
+        <div className={styles.leftSideDiv}>
+          <div className={styles.l_Heading}>
+            Lend a Helping Hand
+          </div>
+          <div className={styles.inputContainer}>
+            <input type={"text"} placeholder={"Name"} className={styles.input}/>
+            <input type={"text"} placeholder={"Email"} className={styles.input}/>
+          </div>
         </div>
       </div>
     </div>
