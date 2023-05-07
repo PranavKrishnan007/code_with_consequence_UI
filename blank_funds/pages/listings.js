a30cb94b4e60973d07a1716474c462959e9185a5
rithulr808 <rithulravichnadra@gmail.com>
2023-05-07 13:37:25 +0530

    added loading

--- blank_funds/pages/listings.js
+++ blank_funds/pages/listings.js
@@ -3,6 +3,7 @@ import Navbar from '@/components/Landing/Navbar.js';
 import OrgCard from '@/components/Listings/OrgCard.js';
 import SideInfoCard from '@/components/Listings/SideInfoCard.js';
 import fetcher from '../utils/fetcher';
+import Loading from "@/components/loading";
 
 export default function listings() {
   const [loading, setLoading] = useState(true);
@@ -28,24 +29,21 @@ export default function listings() {
     setSelectedOrg(org);
   };
 
-  if(loading) return (<div>Loading...</div>);
-
   return (
     <div>
     <Navbar />
-    <div className="container mx-auto px-8 md:px-16" id="listing">
-      
-      <div className="flex flex-row">
-        <div className="w-2/3 pr-6">
-          <OrgCard onOrgSelection={handleOrgSelection} orgs={organisations} />
+      { loading ? <Loading />: (
+        <div className="container mx-auto px-8 md:px-16" id="listing">
+          <div className="flex flex-row">
+            <div className="w-2/3 pr-6">
+              <OrgCard onOrgSelection={handleOrgSelection} orgs={organisations}/>
+            </div>
+            <div className="fixed right-0 top-0 pt-24 h-screen w-1/3 bg-white">
+              {selectedOrg && <SideInfoCard org={selectedOrg}/>}
+            </div>
+          </div>
         </div>
-        <div
-          className="fixed right-0 top-0 pt-24 h-screen w-1/3 bg-white"
-        >
-          {selectedOrg && <SideInfoCard org={selectedOrg} />}
-        </div>
-      </div>
-    </div>
+      )}
     </div>
   );
 }
