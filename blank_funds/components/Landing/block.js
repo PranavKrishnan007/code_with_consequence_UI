f584b57a21ec03f14498e4dad2bc1198d3b0f43c
Sahithya <aythihas2003@gmail.com>
2023-05-07 09:45:57 +0530

    convert tailwind to VANILLA CSS

--- blank_funds/components/Landing/block.js
+++ blank_funds/components/Landing/block.js
@@ -1,31 +1,28 @@
 import React from "react";
-
+import styles from "styles/Block.module.css";
 
 const BLOCK = [
     {
         title: "Inspirational Text",
-        text: "This is some text that I am having to put in because for some reason lorem ipsum is refusing to work can you believe it ? This is ridiculous!"
+        text:
+            "This is some text that I am having to put in because for some reason lorem ipsum is refusing to work can you believe it ? This is ridiculous!",
     },
     {
         title: "Inspirational Text",
-        text: "This is some text that I am having to put in because for some reason lorem ipsum is refusing to work can you believe it ? This is ridiculous!"
+        text:
+            "This is some text that I am having to put in because for some reason lorem ipsum is refusing to work can you believe it ? This is ridiculous!",
     },
-]
+];
 
 export default function Block() {
     return (
-        <div className="py-5">
-            <div className="grid grid-cols-1 gap-10 px-20 md:grid-cols-2">
+        <div className={styles.blockWrapper}>
+            <div className={styles.gridContainer}>
                 {BLOCK.map((block) => (
-                    <div className="flex justify-center flex-col p-8  ">
-                        <span className='font-semibold text-5xl'>Food</span>
-                        <span className='font-bold text-7xl'>Farming</span>
-                        <div className="text-2xl font-semibold">
-                            {block.title}
-                        </div>
-                        <div className="text-xl pt-3">
-                            {block.text}
-                        </div>
+                    <div className={styles.block}>
+                        <span className={styles.subtitle}>Blog</span>
+                        <div className={styles.text2xl}>{block.title}</div>
+                        <div className={styles.textxl}>{block.text}</div>
                     </div>
                 ))}
             </div>
--- blank_funds/components/Listings/OrgCard.js
+++ blank_funds/components/Listings/OrgCard.js
@@ -1,34 +1,38 @@
 import React from "react";
 import Image from "next/image";
 import { GoLocation } from "react-icons/go";
+import styles from "styles/OrgCard.module.css";
 
 export const orgs = [
   {
     name: "UNICEF",
-    details: "the big big components,g big components, Create a platform that allow s  users g big components, Create a platform that allows users Create a  platform that allows users to donate to charities and non-profit  organizations. You can create a platform that features different charities, donation options, and progress tracking.",
+    details:
+      "the big big components,g big components, Create a platform that allow s  users g big components, Create a platform that allows users Create a  platform that allows users to donate to charities and non-profit  organizations. You can create a platform that features different charities, donation options, and progress tracking.",
     location: "pfund",
     image: "/assets/kids_looking_up.png",
     url: "pfund",
-    phone:"8588890427",
-    email:"pfund@gmail.com"
+    phone: "8588890427",
+    email: "pfund@gmail.com",
   },
   {
     name: "pfund",
-    details: "the big big components,g big components, Create a platform that allow s  users g big components, Create a platform that allows users Create a  platform that allows users to donate to charities and non-profit  organizations. You can create a platform that features different charities, donation options, and progress tracking.",
+    details:
+      "the big big components,g big components, Create a platform that allow s  users g big components, Create a platform that allows users Create a  platform that allows users to donate to charities and non-profit  organizations. You can create a platform that features different charities, donation options, and progress tracking.",
     location: "pfund",
     image: "/assets/kids_looking_up.png",
     url: "pfund",
-    phone:"8588890427",
-    email:"pfund@gmail.com"
+    phone: "8588890427",
+    email: "pfund@gmail.com",
   },
   {
     name: "ABC",
-    details: "the big big components,g big components, Create a platform that allow s  users g big components, Create a platform that allows users Create a  platform that allows users to donate to charities and non-profit  organizations. You can create a platform that features different charities, donation options, and progress tracking.",
+    details:
+      "the big big components,g big components, Create a platform that allow s  users g big components, Create a platform that allows users Create a  platform that allows users to donate to charities and non-profit  organizations. You can create a platform that features different charities, donation options, and progress tracking.",
     location: "pfund",
     image: "/assets/kids_looking_up.png",
     url: "pfund",
-    phone:"8588890427",
-    email:"pfund@gmail.com"
+    phone: "8588890427",
+    email: "pfund@gmail.com",
   },
 ];
 
@@ -40,25 +44,25 @@ const OrgCard = ({ onOrgSelection }) => {
   };
 
   return (
-    <div>
+    <div >
       {orgs.map((id) => (
         <div
           key={id.name}
-          className="max-w-4xl bg-background drop-shadow-2xl flex flex-row p-10 space-x-10 my-36"
+          className={styles.orgCard}
         >
           <div>
             <Image src={id.image} width={1024} height={1024} className="" />
           </div>
-          <div className="flex flex-col ">
-            <h3 className="text-4xl font-medium">{id.name}</h3>
-            <p className="tracking-wide line-clamp-3 my-10">{id.details}</p>
-            <div className="flex flex-row justify-between">
-              <div className="flex flex-row items-center ">
-                <GoLocation size={22} />
-                <p className="px-2">{id.location}</p>
+          <div className={styles.orgInfo}>
+            <h3 className={styles.orgname}>{id.name}</h3>
+            <p className={styles.orgdetails}>{id.details}</p>
+            <div className={styles.orgdocationAndReadMore}>
+              <div className={styles.orglocation}>
+                <GoLocation size={22}  />
+                <p className={styles.orgdocationText}>{id.location}</p>
               </div>
               <div
-                className="border-2 border-primary py-3 px-5 hover:bg-primary cursor-pointer hover:shadow-lg"
+                className={styles.orgreadmore}
                 onClick={() => handleClick(id)}
               >
                 Read More
